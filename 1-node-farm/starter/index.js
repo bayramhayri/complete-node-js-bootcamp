5d92adaadc506ab550f9ce07c4ff912794bf1721
bayramhayri <56040100+bayramhayri@users.noreply.github.com>
2021-04-01 09:54:28 +0300

    Add npm packages to node farm add gitignore to repo

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const { URL } = require('url');
-
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 ///////////////////////////////
@@ -47,6 +47,8 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
   const { pathname, searchParams } = new URL(req.url, 'http://127.0.0.1/');
